6dd7066ec4b89d551ecf7db25410b7f2b52222f6
elianaprah <99228580+elianaprah@users.noreply.github.com>
2022-06-22 17:02:15 -0400

    random changes end pt 3

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,22 @@
-const router = require('express').Router()
+const Router = require('express').Router()
 
-router.get('/', (req, res) => {
-    res.send('GET /places')
+// GET /places
+Router.get('/', (req, res) => {
+    let places = [{
+        name: 'Alfredo API',
+        city: 'Portland',
+        state: 'OR',
+        cuisines: 'Italian, South European',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Cannoli Cafe',
+        city: 'Washington',
+        state: 'D.C',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      
+    res.render('places/index', { places })
 })
 
-module.exports = router
+module.exports = Router
